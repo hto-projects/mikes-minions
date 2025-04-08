203b8eda47084ca5503ce76cbe644c5209106c22
User <User@RND-017183.OnBase.net>
2025-04-08 19:24:59 -0400

    Updated score for cavs game

--- Home Screen/script.js
+++ Home Screen/script.js
@@ -8,19 +8,19 @@ document.addEventListener("DOMContentLoaded", () => {
 function fetchLiveGame() {
     // Replace with an actual API call
     document.getElementById("live-game").innerHTML = `
-        <p><strong>Team A</strong> vs <strong>Team B</strong></p>
-        <p>Score: 102 - 98</p>
-        <p>Time Remaining: 3:21 4Q</p>
-        <p>Player of the Game: John Doe - 27 PTS, 8 REB, 5 AST</p>
+        <p><strong>Cavs</strong> vs <strong>Bulls</strong></p>
+        <p>Score: 13 - 12</p>
+        <p>Time Remaining: 5:28 1Q</p>
+        <p>Player of the Game: N/A</p>
     `;
 }
 
 function fetchMVPStats() {
     // Replace with an actual API call
     let mvpPlayers = [
-        { name: "shai", team: "Lakers", ppg: 28.3, rpg: 7.2, apg: 8.1 },
-        { name: "Jokic", team: "Nuggets", ppg: 26.5, rpg: 11.8, apg: 9.4 },
-        { name: "Giannis", team: "Bucks", ppg: 29.9, rpg: 12.2, apg: 5.7 }
+        { name: "Shai", team: "Thunder", ppg: 32.6, rpg: 5.0, apg: 6.4 },
+        { name: "Jokic", team: "Nuggets", ppg: 30.0, rpg: 12.8, apg: 10.2 },
+        { name: "Giannis", team: "Bucks", ppg: 30.5, rpg: 11.9, apg: 6.3 }
     ];
 
     let list = document.getElementById("mvp-list");
@@ -41,7 +41,7 @@ function fetchAIPrediction() {
 function fetchNews() {
     // Replace with an API call
     document.getElementById("news-feed").innerHTML = `
-        <p><strong>Breaking:</strong> Star player injured in tonight's game. <a href="https://www.nba.com/news">Read more</a></p>
-        <p><strong>Trade Rumors:</strong> Team X looking to acquire Player Y. <a href="#">Read more</a></p>
+        <p><strong>Breaking:</strong> Nuggets Fire Head Coach Michael Malone and GM Calvin Booth. <a href="https://www.nytimes.com/athletic/6264653/2025/04/08/michael-malone-fired-nuggets-head-coach/">Read more</a></p>
+        <p><strong>Trade Rumors:</strong> Phoenix Suns are looking to acquire a large haul for Kevin Durant even though he doesn't want to leave. <a href="https://www.espn.com/nba/story/_/id/44540677/stake-nba-west-storylines-matchups-playoffs">Read more</a></p>
     `;
 }
